cdaa81d2d4e619a6b9f99d9b4b7e064ee8f64201
vaibjain11 <141463293+vaibjain11@users.noreply.github.com>
2024-07-16 04:27:26 -0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,10 @@
+/* Assignment 1: Space Minesweeper
+ * CSCI 4611, Spring 2023, University of Minnesota
+ * Instructor: Evan Suma Rosenberg <suma@umn.edu>
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ */ 
+
+import { SpaceMinesweeper } from './SpaceMinesweeper'
+
+const app = new SpaceMinesweeper();
+app.start();
